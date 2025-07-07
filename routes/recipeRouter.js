f0004f2dee12eb375fcbb3c44fd3c1a4fd3a5582
anastasiia <aizatsana15@gmail.com>
2025-07-07 23:22:50 +0300

    Added favorite endpoind; updated services

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -1,5 +1,16 @@
 import express from "express";
+import authenticate from "../middlewares/authenticate.js";
+import controllers from "../controllers/recipeController.js";
+import { updateStatusSchema } from "../schemas/contactsSchemas.js";
 
 const recipeRouter = express.Router();
+recipeRouter.use(authenticate);
+
+recipeRouter.get("/", controllers.getAllRecipes);
+recipeRouter.patch(
+  "/:id/favorite",
+  validateBody(updateStatusSchema),
+  controllers.updateRecipeStatus
+);
 
 export default recipeRouter;
