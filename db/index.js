4ad62d4e44e50371ef5732f7ab0041fe1184621a
Natalia Kucheriava <nkucheryava@gmail.com>
2025-07-08 23:37:15 +0200

    be for users/follow partly done

---
+++ db/index.js
@@ -0,0 +1,24 @@
+import User from "./User.js";
+import Follow from "./Follow.js";
+
+
+Follow.belongsTo(User, { as: "follower", foreignKey: "followerId" });
+Follow.belongsTo(User, { as: "following", foreignKey: "followingId" });
+
+User.belongsToMany(User, {
+  as: "Followers",
+  through: Follow,
+  foreignKey: "followingId",
+  otherKey: "followerId",
+});
+
+User.belongsToMany(User, {
+  as: "Following",
+  through: Follow,
+  foreignKey: "followerId",
+  otherKey: "followingId",
+});
+
+
+
+export { User, Follow };
