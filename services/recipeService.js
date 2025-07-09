b5fd8efbb063d2292e35b1220a3c9941b9dc985f
kateryna_kornysheva <k.kornysheva@betinvest.com>
2025-07-09 09:47:12 +0200

    done

--- services/recipeService.js
+++ services/recipeService.js
@@ -1,11 +1,7 @@
-import Recipe from "../db/Recipe";
+import Recipe from "../db/Recipe.js";
 
 export const allRecipes = async (query) => {
   return Recipe.findAll({ where: query });
-};
-
-export const getContactById = async (query) => {
-  return Recipe.findOne({ where: query });
 };
 
 export const updateRecipeStatus = async (query, data) => {
