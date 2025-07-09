7550ac37c11c2d17eb8eb840b01c341b6d3af038
innasnegurova <innasnegurova@nxte.nl>
2025-07-09 15:44:11 +0200

    fix: recipeRouter.js

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import authenticate from '../middlewares/authenticate.js';
 import controllers from '../controllers/recipeController.js';
-// import { updateStatusSchema } from "../schemas/contactsSchemas.js";
+import { updateStatusSchema } from '../schemas/recipeSchemas.js';
 import validateBody from '../decorators/validateBody.js';
 
 const recipeRouter = express.Router();
 recipeRouter.use(authenticate);
 
 recipeRouter.get('/', controllers.getAllRecipes);
-// recipeRouter.patch(
-//   "/:id/favorite",
-//   validateBody(updateStatusSchema),
-//   controllers.updateRecipeStatus
-// );
+recipeRouter.patch(
+  '/:id/favorite',
+  validateBody(updateStatusSchema),
+  controllers.updateRecipeStatus
+);
 
 export default recipeRouter;
