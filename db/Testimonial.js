e4e388b4b2bc4f8a6086e26c33e0bb836715da19
Anatolii Hrebynakha <hrebynakha@gmail.com>
2025-07-12 18:55:33 +0300

    temp fix import for testimonials

--- db/Testimonial.js
+++ db/Testimonial.js
@@ -1,27 +1,29 @@
 import { DataTypes } from 'sequelize';
 import sequelize from './sequelize.js';
+import User from './User.js';
+import Recipe from './Recipe.js';
 
 const Testimonial = sequelize.define('testimonial', {
-    owner: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: User,
-          key: 'id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-      },
-      recipeId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: Recipe,
-          key: 'id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-      },
+  owner: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  recipeId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: Recipe,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
   testimonial: {
     type: DataTypes.TEXT,
     allowNull: false,
