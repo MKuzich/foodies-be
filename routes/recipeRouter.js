aa462d12b7bdb82daacb778d2430cf3c01d43804
Natalia Kucheriava <nkucheryava@gmail.com>
2025-07-13 15:22:12 +0200

    swagger added

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -5,11 +5,75 @@ import validateBody from '../decorators/validateBody.js';
 
 const recipeRouter = express.Router();
 
+
+/**
+ * @swagger
+ * /recipes:
+ *   get:
+ *     summary: Get all recipes
+ *     tags: [Recipes]
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: Page number
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: Items per page
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         description: Filter by category
+ *     responses:
+ *       200:
+ *         description: List of recipes
+ *       500:
+ *         description: Server error
+ */
 recipeRouter.get('/', recipesControllers.getAllRecipes);
+
+/**
+ * @swagger
+ * /recipes/{id}/favorite:
+ *   patch:
+ *     summary: Add or remove a recipe from favorites
+ *     tags: [Recipes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               favorite:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Favorite status updated
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Recipe not found
+ */
 recipeRouter.patch(
   '/:id/favorite',
   validateBody(updateStatusSchema),
   recipesControllers.updateRecipeStatus
 );
+
 
 export default recipeRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,37 +8,133 @@ import validateId from '../middlewares/validateId.js';
 
 const userRouter = express.Router();
 
-userRouter.get(
-  '/following',
-  authenticate,
-  followControllers.getFollowingController
-);
 
-userRouter.get(
-  '/:id/followers',
-  authenticate,
-  validateId,
-  followControllers.getFollowersController
-);
+/**
+ * @swagger
+ * /users/following:
+ *   get:
+ *     summary: Get list of users the current user is following
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: Page number
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: Number of items per page
+ *     responses:
+ *       200:
+ *         description: List of followed users
+ *       401:
+ *         description: Unauthorized
+ */
+userRouter.get('/following', authenticate, followControllers.getFollowingController);
 
-userRouter.post(
-  '/:id/follow',
-  authenticate,
-  validateId,
-  followControllers.followUserController
-);
+/**
+ * @swagger
+ * /users/{id}/followers:
+ *   get:
+ *     summary: Get list of followers for the specified user
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: List of followers
+ *       404:
+ *         description: User not found
+ */
+userRouter.get('/:id/followers', authenticate, validateId, followControllers.getFollowersController);
 
-userRouter.delete(
-  '/:id/unfollow',
-  validateId,
-  authenticate,
-  followControllers.unfollowUserController
-);
+/**
+ * @swagger
+ * /users/{id}/follow:
+ *   post:
+ *     summary: Follow the specified user
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       201:
+ *         description: Followed successfully
+ *       400:
+ *         description: Cannot follow yourself
+ *       404:
+ *         description: User not found
+ *       409:
+ *         description: Already following this user
+ */
+userRouter.post('/:id/follow', authenticate, validateId, followControllers.followUserController);
 
-userRouter.get(
-  '/:id',
-  authenticate,
-  userController.getUserInfoController
-);
+/**
+ * @swagger
+ * /users/{id}/unfollow:
+ *   delete:
+ *     summary: Unfollow the specified user
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Unfollowed successfully
+ *       404:
+ *         description: Not following this user or user not found
+ */
+userRouter.delete('/:id/unfollow', authenticate, validateId, followControllers.unfollowUserController);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get user profile details
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: User profile details
+ *       404:
+ *         description: User not found
+ */
+userRouter.get('/:id', authenticate, userController.getUserInfoController);
+
 
 export default userRouter;
