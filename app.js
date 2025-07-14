c020e420db988eb10a3154001b04ce7dd88db49b
Natalia Kucheriava <nkucheryava@gmail.com>
2025-07-14 21:13:45 +0200

    swagger update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
